50945079a33d085a8ad0cf93ce2e6d350975c10a
coderinlove-dev <regalgavde@gmail.com>
2025-09-05 19:10:43 +0530

    Deleted old  routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const authenticateToken = require('../middleware/authMiddleware'); // your JWT checker
-
-router.get('/me', authenticateToken, userController.getUserProfile);
-
-module.exports = router;
